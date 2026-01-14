fe23a916283c7642cf958341f2cf07c09464b5ce
Maftuh Standalone <maftuh.standalone@example.com>
2026-01-14 09:31:15 +0700

    perbaikan tampilan

--- script.js
+++ script.js
@@ -178,12 +178,4 @@
         loadData();
         tampil();
 
-        // --- BAGIAN NAVIGASI HAMBURGER ---
-        const hamburger = document.getElementById("hamburger");
-        const navMenu = document.getElementById("navMenu");
-
-        if (hamburger && navMenu) {
-            hamburger.addEventListener("click", () => {
-                navMenu.classList.toggle("active");
-            });
-        }
+      
